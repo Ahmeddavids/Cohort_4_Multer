5545775c75ff99414c9a697d311951f59db30f64
ahmed isiaka <ahmeddavids6@gmail.com>
2024-07-22 15:13:48 +0100

    Uodated the base URL

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json())
 
 app.use('/uploads', express.static('uploads'))
 
-app.use('/api/v1/users', userRouter)
+app.use('/api/v1/user', userRouter)
 
 app.listen(8000, () => {
     console.log(`server running on PORT: 8000`);
