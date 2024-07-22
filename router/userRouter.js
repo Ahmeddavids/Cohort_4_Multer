31cea703440c158da835afd009ca8092af74da63
ahmed isiaka <ahmeddavids6@gmail.com>
2024-07-22 15:13:25 +0100

    Added the delete route

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { signUp, getOne, getAll, updateUser } = require('../controller/userController')
+const { signUp, getOne, getAll, updateUser, deleteUser } = require('../controller/userController')
 const upload = require('../utils/multer.js')
 const router = express.Router()
 
@@ -10,5 +10,7 @@ router.get('/one/:id', getOne)
 router.get('/all', getAll)
 
 router.put('/update/:id', upload.single('image'), updateUser)
+
+router.delete('/delete/:id',  deleteUser)
 
 module.exports = router;
